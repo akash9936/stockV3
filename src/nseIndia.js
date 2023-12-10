f466f46ca87255c3e481c4e2667a0fe89c98547b
akashsingh <akashsingh@propelld.com>
2023-12-10 23:09:38 +0530

    Added Number of records and filte date and time

--- src/nseIndia.js
+++ src/nseIndia.js
@@ -43,7 +43,7 @@ const mongooseOptions = {
             } catch (error) {
                 console.error('Error fetching or inserting data:', error.message);
             }
-        }, 10000);
+        }, 60000);
 
         // Set up the Express server
         app.use(cors());
