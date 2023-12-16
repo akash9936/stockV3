5ea89c7928f98698282a75266ba20ddf932162ac
akashsingh <akashsingh@propelld.com>
2023-12-17 04:45:09 +0530

    Added isInTradingHours condition

--- src/nseIndia.js
+++ src/nseIndia.js
@@ -24,26 +24,32 @@ const mongooseOptions = {
         // Set up the interval after the connection is established
         setInterval(async () => {
             try {
-                const data = await fetchData();
-
-                if (data) {
-                    const simplifiedData = {
-                        timestamp: data.timestamp,
-                        data: data.data.map(stock => ({
-                            symbol: stock.symbol,
-                            lastPrice: stock.lastPrice
-                        }))
-                    };
-
-                  await NSE50Data.collection.insertOne(simplifiedData);
-                    console.log('Data inserted into MongoDB.');
-                } else {
-                    console.error('Error: Data is not available.');
+                if (!isInTradingHours()) {
+                    console.log("Market is not open")
                 }
-            } catch (error) {
+                else {
+                    const data = await fetchData();
+
+                    if (data) {
+                        const simplifiedData = {
+                            timestamp: data.timestamp,
+                            data: data.data.map(stock => ({
+                                symbol: stock.symbol,
+                                lastPrice: stock.lastPrice
+                            }))
+                        };
+
+                        await NSE50Data.collection.insertOne(simplifiedData);
+                        console.log('Data inserted into MongoDB.');
+                    } else {
+                        console.error('Error: Data is not available.');
+                    }
+                }
+            }
+            catch (error) {
                 console.error('Error fetching or inserting data:', error.message);
             }
-        }, 10000);
+        }, 30000);
 
         // Set up the Express server
         app.use(cors());
@@ -59,3 +65,13 @@ const mongooseOptions = {
         console.error('MongoDB connection error:', error);
     }
 })();
+
+const isInTradingHours = () => {
+    const now = new Date();
+    const dayOfWeek = now.getDay(); // Sunday is 0, Monday is 1, ..., Saturday is 6
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+
+    // Check if it's a weekday (Monday to Friday) and within trading hours
+    return dayOfWeek >= 1 && dayOfWeek <= 5 && hours >= 9 && hours < 15 && (hours !== 15 || minutes <= 35);
+};
