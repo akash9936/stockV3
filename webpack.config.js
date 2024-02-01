35279f99e48eea957e8bbce5a71957403ab06825
akashsingh <akashsingh@propelld.com>
2024-02-02 00:33:56 +0530

    Added koyeb Deploy config

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,29 @@
-const path = require('path'); 
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 module.exports = {
     entry: './src/nseIndiaV2.js',
     output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
     },
     resolve: {
         fallback: {
-          path: require.resolve('path-browserify'),
-          fs: false, // You may not need a polyfill for fs in a browser environment
-          stream: require.resolve('stream-browserify'),
-          zlib: require.resolve('browserify-zlib'),
-          querystring: require.resolve('querystring-es3'),
-          crypto: require.resolve('crypto-browserify'),
-          http: require.resolve('stream-http'),
-          net: false, // You may not need a polyfill for net in a browser environment
-          async_hooks: false, // You may not need a polyfill for async_hooks in a browser environment
+            path: require.resolve('path-browserify'),
+            fs: false,
+            stream: require.resolve('stream-browserify'),
+            zlib: require.resolve('browserify-zlib'),
+            querystring: require.resolve('querystring-es3'),
+            crypto: require.resolve('crypto-browserify'),
+            http: require.resolve('stream-http'),
+            net: false,
+            async_hooks: false,
+            os: require.resolve('os-browserify/browser'),
         },
-      },
+    },
+    devtool: 'source-map',
+    plugins: [
+        new CleanWebpackPlugin(),
+    ],
     // ... other configurations
-  };
-  
+};
