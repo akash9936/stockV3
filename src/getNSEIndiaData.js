6ed0bdcb41a24ced279a613ae708bbe4d1254366
akashsingh <akashsingh@propelld.com>
2024-09-17 22:31:33 +0530

    Added for Websocket netlify

--- src/getNSEIndiaData.js
+++ src/getNSEIndiaData.js
@@ -25,7 +25,7 @@ function fetchData() {
                 'sec-fetch-site': 'same-origin',
                 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
             },
-            timeout:3000
+            timeout:300000
         };
 
        await axios.request(config)
