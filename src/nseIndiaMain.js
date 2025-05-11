bd3dfdbf5b5d4acb17c6d6a3e425c1b86428693d
akashsingh <akashsingh@propelld.com>
2025-05-11 20:32:47 +0530

    time change

--- src/nseIndiaMain.js
+++ src/nseIndiaMain.js
@@ -16,7 +16,7 @@ const { createAlertMessages } = require('./Utills/CreateAlertMessage')
 
 const app = express();
 dotenv.config();
-const fetchDataCronTime = 300000;
+const fetchDataCronTime = 30000;
 const port = 6002;
 const mongooseOptions = {
     useNewUrlParser: true,
